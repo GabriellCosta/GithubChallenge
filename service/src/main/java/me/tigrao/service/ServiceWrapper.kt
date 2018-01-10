ddd8d56eda1f257b42297143428e61414ac29723
Gabriel Costa <gabrielcosta08@gmail.com>
2018-01-10 08:59:31 -0200

    Add Wrapper for retrofit service

---
+++ service/src/main/java/me/tigrao/service/ServiceWrapper.kt
@@ -0,0 +1,32 @@
+package me.tigrao.service
+
+import android.arch.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class ServiceWrapper internal constructor(private val api: ServiceEndpoint) {
+
+    lateinit var valueObserver: MutableLiveData<RepositorieDTO>
+    lateinit var errorObserver: MutableLiveData<String>
+
+    fun search(language: String, sort: String, page: Int) {
+        api.search(language, sort, page).enqueue(object : InnerCallback<RepositorieDTO>() {
+
+            override fun onResponse(call: Call<RepositorieDTO>, response: Response<RepositorieDTO>) {
+                EnqueueHandler(valueObserver, errorObserver).handler(response)
+            }
+
+        })
+    }
+
+    private abstract class InnerCallback<T> : Callback<T> {
+
+        override fun onFailure(call: Call<T>?, t: Throwable?) {
+
+        }
+
+    }
+
+}
