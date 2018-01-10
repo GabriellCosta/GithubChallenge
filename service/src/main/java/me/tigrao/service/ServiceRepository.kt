46964105478d2bdcda806fd4731ed3755798a987
Gabriel Costa <gabrielcosta08@gmail.com>
2018-01-10 09:04:43 -0200

    Update ServiceRepositoryImpl
    Fix tests and update to use ServiceWrapper

--- service/src/main/java/me/tigrao/service/ServiceRepository.kt
+++ service/src/main/java/me/tigrao/service/ServiceRepository.kt
@@ -1,22 +1,27 @@
 package me.tigrao.service
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 interface ServiceRepository {
 
-    fun fetchRepositories(language: String, sort: String, page: Int)
+    fun fetchRepositories(language: String, sort: String, page: Int): LiveData<RepositorieDTO>
 
     fun fetchPullRequest(user: String, repositoy: String)
 
 }
 
-class ServiceRepositoryImpl internal constructor(private val api: ServiceEndpoint): ServiceRepository {
 
-    val valueObserver: MutableLiveData<RepositorieDTO> = MutableLiveData()
-    val errorObserver: MutableLiveData<String> = MutableLiveData()
+class ServiceRepositoryImpl internal constructor(private val serviceWrapper: ServiceWrapper):
+        ServiceRepository {
+
+    private val valueObserver: MutableLiveData<RepositorieDTO> = MutableLiveData()
+    private val errorObserver: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        serviceWrapper.errorObserver = errorObserver
+        serviceWrapper.valueObserver = valueObserver
+    }
 
     companion object {
 
@@ -25,40 +30,17 @@ class ServiceRepositoryImpl internal constructor(private val api: ServiceEndpoin
         fun newInstance(): ServiceRepository {
             val create = RetrofitBuilder.build(GITHUB_ENDPOINT)
                     .create(ServiceEndpoint::class.java)
-            return ServiceRepositoryImpl(create)
+            return ServiceRepositoryImpl(ServiceWrapper(create))
         }
     }
 
-    override fun fetchRepositories(language: String, sort: String, page: Int) {
-        api.search(language, sort, page).enqueue(object : InnerCallback<RepositorieDTO>() {
-
-            override fun onResponse(call: Call<RepositorieDTO>, response: Response<RepositorieDTO>) {
-                if (response.isSuccessful)
-                    valueObserver.value = response.body()
-                else
-                    errorObserver.value = "Sem valor"
-            }
-
-        })
+    override fun fetchRepositories(language: String, sort: String, page: Int): LiveData<RepositorieDTO> {
+        serviceWrapper.search(language, sort, page)
+        return valueObserver
     }
 
-    override fun fetchPullRequest(user: String, repositoy: String) {
-        api.fetchPullRequests(user, repositoy)
-                .enqueue(object : InnerCallback<List<PullRequestVO>>() {
-                    override fun onResponse(call: Call<List<PullRequestVO>>?, response: Response<List<PullRequestVO>>?) {
-
-                    }
-
-                })
-    }
-
-    private abstract class InnerCallback<T>: Callback<T> {
-
-        override fun onFailure(call: Call<T>?, t: Throwable?) {
-
-        }
+    override fun fetchPullRequest(user: String, repository: String) {
 
     }
-
 
 }
--- service/src/test/kotlin/me/tigrao/service/ServiceRepositoryImplSpec.kt
+++ service/src/test/kotlin/me/tigrao/service/ServiceRepositoryImplSpec.kt
@@ -1,37 +1,32 @@
 package me.tigrao.service
 
 import android.arch.lifecycle.Observer
-import com.google.gson.Gson
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import retrofit2.mock.MockRetrofit
+import org.mockito.Mockito
 
 class ServiceRepositoryImplSpec: Spek({
 
     given("a ServiceRespository implementation") {
         val repositorieDTO = readFile("repositories_page_1.json", RepositorieDTO::class.java)
         val observer = mock<Observer<RepositorieDTO>>()
-        val build = RetrofitBuilder.build("http://api.github.com")
-        val mockRetrofit = MockRetrofit.Builder(build)
-                .build()
-        val create = mockRetrofit.create(ServiceEndpoint::class.java)
-        create.returningResponse(Gson().toJson(repositorieDTO))
-        val newInstance = ServiceRepositoryImpl(build.create(ServiceEndpoint::class.java))
-        newInstance.valueObserver.observeForever(observer)
+        val serviceWrapper = mock<ServiceWrapper>()
+        val newInstance = ServiceRepositoryImpl(serviceWrapper)
+        Mockito.`when`(serviceWrapper.search("java", "stars", 1)).then {
+            observer.onChanged(repositorieDTO)
+        }
 
         on("repositorie fetch") {
             newInstance.fetchRepositories("java", "stars", 1)
+                    .observeForever(observer)
 
-
-            it("should have 30 items") {
+            it("should observe the correct value") {
                 verify(observer).onChanged(repositorieDTO)
             }
-
-
 
         }
 
